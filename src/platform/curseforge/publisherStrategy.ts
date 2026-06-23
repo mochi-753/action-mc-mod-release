f8b8ad5b15f32472e98cb6e0bda1e2bdc04dbb58
Mochi753 <179483962+mochi-753@users.noreply.github.com>
2026-06-23 23:21:30 +0900

    refactor!: Artifact[]に変更

--- src/platform/curseforge/publisherStrategy.ts
+++ src/platform/curseforge/publisherStrategy.ts
@@ -12,12 +12,17 @@ export class CurseForgePublisherStrategy implements PublisherStrategy {
     ) {
     }
 
-    async publish(inputs: { metadata: CurseForgeMetadata, file: Artifact }): Promise<Response> {
+    async publish(inputs: { metadata: CurseForgeMetadata, files: Artifact[] }): Promise<Response> {
+        if (inputs.files.length !== 1) {
+            throw new Error('The CurseForge release process, which allows only one file to be uploaded at a time, received multiple files.')
+        }
+
         const curseforgeRequest = new CurseForgeRequest(this.token, this.userAgent)
-        const formData = curseforgeRequest.buildPublishFormData({ metadata: inputs.metadata, file: inputs.file })
+        const curseforgeApiClient = new CurseForgeApiClient('https://minecraft.curseforge.com', await this.projectID)
+
+        const formData = curseforgeRequest.buildPublishFormData({ metadata: inputs.metadata, file: inputs.files[0]! })
         const requestInit = curseforgeRequest.buildRequestInit({ method: 'POST', formData: formData })
 
-        const curseforgeApiClient = new CurseForgeApiClient('https://minecraft.curseforge.com', await this.projectID)
         return curseforgeApiClient.publish(requestInit);
     }
 }
--- src/platform/strategy.ts
+++ src/platform/strategy.ts
@@ -2,5 +2,5 @@ import type { Artifact } from "./common/artifact.js";
 import type { CurseForgeMetadata } from "./curseforge/metadata.js";
 
 export interface PublisherStrategy {
-    publish(inputs: { metadata: CurseForgeMetadata, file: Artifact }): Promise<Response>
+    publish(inputs: { metadata: CurseForgeMetadata, files: Artifact[] }): Promise<Response>
 }
